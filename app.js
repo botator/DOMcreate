b53f7439c14e999cbea2a0ece52421795fd78610
Bástyai Botond <64700017+botator@users.noreply.github.com>
2024-11-27 09:34:36 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -7,12 +7,58 @@ let globalState = {};
 const field1 = renderField({
   state: globalState,
   id: 'field1',
-  type: 'text'
+  type: 'text',
+  label: 'szöveg: ',
+  labelOnTop: ''
 });
+
 const field2 = renderField({
-    state: globalState,
-    id: 'field2',
-    type: 'number'
-  });
+  state: globalState,
+  id: 'field2',
+  type: 'text',
+  label: '',
+  labelOnTop: 'szöveg: '
+});
+const field3 = renderField({
+  state: globalState,
+  id: 'field3',
+  type: 'number',
+  label: '',
+  labelOnTop: 'szöveg: '
+});
+const field4 = renderField({
+  state: globalState,
+  id: 'field4',
+  type: 'number',
+  label: 'szöveg: ',
+  labelOnTop: ''
+});
+const field5 = renderField({
+  state: globalState,
+  id: 'field5',
+  type: '',
+  label: '',
+  labelOnTop: ''
+});
+const field6 = renderField({
+  state: globalState,
+  id: 'field6',
+  type: 'email',
+  label: '',
+  labelOnTop: 'szöveg: '
+});
+const field7 = renderField({
+  state: globalState,
+  id: 'field7',
+  type: 'email',
+  label: 'szöveg: ',
+  labelOnTop: ''
+});
 document.getElementById('app').appendChild(field1);
 document.getElementById('app').appendChild(field2);
+document.getElementById('app').appendChild(field3);
+document.getElementById('app').appendChild(field4);
+document.getElementById('app').appendChild(field5);
+document.getElementById('app').appendChild(field6);
+document.getElementById('app').appendChild(field7);
+
--- field.js
+++ field.js
@@ -1,15 +1,134 @@
 export function renderField(config) {
-    let input = document.createElement('input');
-    input.type = config.type;
-    input.id = config.id;
+  let field;
+  switch(config.type){
+    case 'text':
+      field = renderText(config);
+      break;
+    case 'number':
+      field = renderNumber(config);
+      break;
+    case 'email':
+      field = renderEmail(config);
+      break;
+    case '':
+      field = renderDef(config);
+      break;
+  }
+
+  return field
+
+ 
+}
+
+function renderText(config) {
+  let label = document.createElement('label');
+  let input = document.createElement('input');
+  let div = document.createElement('div');
+  let br = document.createElement('br')
+  if(config.label == ""){
+    label.textContent = config.labelOnTop;
+    div.appendChild(label);
+    div.appendChild(br)
+    div.appendChild(input);
+  }
+  if(config.labelOnTop == ""){
+    label.textContent = config.label;
+    div.appendChild(label);
+    div.appendChild(input);
+  }
+  input.type = config.type;
+  input.id = config.id
+  
+  
+  input.addEventListener('input', () => {
+    config.state[config.id] = input.value;
+    //console.log(JSON.stringify(config.state));
+    console.log(config.state)
+  }); 
+  return div;
+}
+
+function renderNumber(config) {
+  let input = document.createElement('input');
+  let label = document.createElement('label');
+  let div = document.createElement('div');
+  let br = document.createElement('br')
+  if(config.label == ""){
+    label.textContent = config.labelOnTop;
+    div.appendChild(label);
+    div.appendChild(br)
+    div.appendChild(input);
+  }
+  if(config.labelOnTop == ""){
+    label.textContent = config.label;
+    div.appendChild(label);
+    div.appendChild(input);
+  }
+  
+  input.type = 'number';
+  input.id = config.id;
+
+  input.addEventListener('input', () => {
+    config.state[config.id] = input.value;
+    //console.log(JSON.stringify(config.state));
+    console.log(config.state)
+  }); 
 
   
-    input.addEventListener('input', () => {
-      config.state[config.id] = input.value;
-      //console.log(JSON.stringify(config.state));
-        console.log(config.state)
-    });
-  
-    return input;
+  return div;
 }
+
+function renderEmail(config) {
+  const parentDiv = document.createElement('div')
+  const br = document.createElement('br');
+  let label = document.createElement('label');
+  label.textContent = label.config 
+
+  const localInput = document.createElement('input')
+  localInput.type = "text"
+
+  const at = document.createElement('span');
+  at.textContent = '@';
+
+  const domainInput = document.createElement('input')
+  domainInput.type = "text"
+
+  const emailParts = config.state[config.id] ? config.state[config.id].split('@') : [];
+  localInput.value = emailParts[0] || "";
+  domainInput.value = emailParts[1] || "";
+
+  if(config.label == ""){
+    label.textContent = config.labelOnTop;
+    parentDiv.append(label,br,localInput, at, domainInput)
+  }
+  if(config.labelOnTop == ""){
+    label.textContent = config.label;
+    parentDiv.append(label,localInput, at, domainInput)
+  }
   
+
+  localInput.addEventListener('input', () => {
+    config.state[config.id] = `${localInput.value}@${domainInput.value}`;
+      console.log(JSON.stringify(config.state));
+  });
+
+  domainInput.addEventListener('input', () => {
+    config.state[config.id] = `${localInput.value}@${domainInput.value}`;
+      console.log(JSON.stringify(config.state));
+  });
+
+  return parentDiv
+}
+
+function renderDef(config) {
+  let div = document.createElement('div');
+  div.id = config.id;
+
+  div.addEventListener('div', () => {
+    config.state[config.id] = div.value;
+    //console.log(JSON.stringify(config.state));
+    console.log(config.state)
+  });
+
+  return div;
+}
